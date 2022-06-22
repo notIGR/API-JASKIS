7f8f87661f5be43409f14f7d81866409dfb86c3e
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-06-22 15:17:29 -0400

    added data

--- commands.js
+++ commands.js
@@ -3,17 +3,85 @@
 
 // GETTING STARTED
 // 1. Create a database called jaskis
-
+use jaskis
 
 // 2. Create a collection called bounties
+debugger.createCollection("bounties")
 
-
+show collections
 // ADD THE ANIMAL BOUNTIES
 // 1. Insert the given "Thanoceros" bounty object
+db.bounties.insertOne( {
+    name: "Thanoceros",
+    species: "Rhinoceros",
+    loaction: "Grasslands",
+    wantedFor: "Eating too much grass",
+    client: "Songbird",
+  reward: 10000,
+  captured: false } )
 
+  db.bounties.find()
 
 // 2. Query for all bounties in the bounties collection
+db.bounties.insertMany(
+[
+  {
+    "name": "Lokinkajou",
+    "species": "Kinkajou",
+    "location": "Tropical rainforest",
+    "wantedFor": "Partying too late at night",
+    "client": "White tiger",
+    "reward": 1000,
+    "captured": false
+  },
+  {
+    "name": "Nebullama",
+    "species": "Llama",
+    "location": "Grasslands",
+    "wantedFor": "Drinking all the water from the ocean",
+    "client": "Songbird",
+    "reward": 2500,
+    "captured": false
+  },
+  {
+    "name": "Polarwind",
+    "species": "Polar Bear",
+    "location": "Arctic",
+    "wantedFor": "Not hibernating",
+    "client": "Sabertooth",
+    "reward": 4000,
+    "captured": false
+  },
+  {
+    "name": "Wrecking Crows",
+    "species": "Crow",
+    "location": "Grasslands",
+    "wantedFor": "Cawing too loudly",
+    "client": "Red wolf",
+    "reward": 40000,
+    "captured": false
+  },
+  {
+    "name": "Grandhog",
+    "species": "Groundhog",
+    "location": "Woodlands",
+    "wantedFor": "Not coming out of the hole on time and prolonging winter",
+    "client": "Songbird",
+    "reward": 50000,
+    "captured": false
+  },
+  {
+    "name": "Grim Panda",
+    "species": "Giant Panda",
+    "location": "Temperate forest",
+    "wantedFor": "Eating all the bamboo",
+    "client": "Red wolf",
+    "reward": 5000,
+    "captured": false
+  }
+]  )
 
+db.bounties.find()
 
 // 3. Insert many bounties at once using the given objects
 
